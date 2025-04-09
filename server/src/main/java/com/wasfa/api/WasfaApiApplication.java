afd0062a8f0c275358f08302b7ac48f05805d232
LORDyyyyy <lordy.khalid@gmail.com>
2025-04-09 18:00:00 +0200

    Project starting structure and files

---
+++ server/src/main/java/com/wasfa/api/WasfaApiApplication.java
@@ -0,0 +1,13 @@
+package com.wasfa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasfaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasfaApiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/wasfa/api/WasfaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasfa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasfaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
