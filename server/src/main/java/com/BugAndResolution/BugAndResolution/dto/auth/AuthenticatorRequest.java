c94dbf1a76117b461ecc136e9d5da5bac0c18295
mostsfa538 <mostafa51moktar@gmail.com>
2025-04-30 13:44:56 +0300

    change authenticate from username to email

--- server/src/main/java/com/BugAndResolution/BugAndResolution/dto/auth/AuthenticatorRequest.java
+++ server/src/main/java/com/BugAndResolution/BugAndResolution/dto/auth/AuthenticatorRequest.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticatorRequest {
-    private String username;
+    private String email;
     private String password;
 }
